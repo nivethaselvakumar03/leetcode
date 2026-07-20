d7402f4c17c193f25a78805f652fbd4681533f9f
nivethaselvakumar03 <nivethaselvakumar266@gmail.com>
2026-07-20 10:59:06 +0530

    Add files via upload

---
+++ Lastword.java
@@ -0,0 +1,8 @@
+class Lastword {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        String[]words = s.split(" ");
+        return words[words.length - 1].length();
+        
+    }
+}
