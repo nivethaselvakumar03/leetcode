38d9bff1f71a45073adb6a236b02d9e7abeb0246
nivethaselvakumar03 <nivethaselvakumar266@gmail.com>
2026-07-20 08:36:55 +0530

    Add files via upload

---
+++ Straight.java
@@ -0,0 +1,25 @@
+class Straigh {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int x0 = coordinates[0][0];
+        int y0 = coordinates[0][1];
+
+        int x1 = coordinates[1][0];
+        int y1 = coordinates[1][1];
+
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+
+        // Check every remaining point against the line
+        for (int i = 2; i < coordinates.length; i++) {
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+
+            // Avoid division: (y - y0) / (x - x0) must equal dy / dx
+            if ((y - y0) * dx != (x - x0) * dy) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ validnum.java
@@ -0,0 +1,40 @@
+class validnum {
+    public boolean isNumber(String s) {
+        boolean seenDigit = false;
+        boolean seenDot = false;
+        boolean seenExponent = false;
+        boolean digitAfterExponent = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                seenDigit = true;
+                digitAfterExponent = true;
+            }
+            else if (c == '.') {
+                if (seenDot || seenExponent)
+                    return false;
+                seenDot = true;
+            }
+            else if (c == 'e' || c == 'E') {
+                if (seenExponent || !seenDigit)
+                    return false;
+
+                seenExponent = true;
+                digitAfterExponent = false;
+            }
+            else if (c == '+' || c == '-') {
+                if (i != 0 &&
+                    s.charAt(i - 1) != 'e' &&
+                    s.charAt(i - 1) != 'E')
+                    return false;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return seenDigit && digitAfterExponent;
+    }
+}
